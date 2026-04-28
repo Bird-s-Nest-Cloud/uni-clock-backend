e5f109590dc35b9a12fa57a4926cf9158506892b
Asifur Rahaman <asifrahamannub66@gmail.com>
2026-04-29 00:47:54 +0600

    update order advance

--- bike_shop/settings.py
+++ bike_shop/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'bike_shop.wsgi.application'
 
 # TEMPORARY: Using SQLite for development
 # To switch back to MySQL, comment out the SQLite config and uncomment the MySQL config below
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # MySQL Configuration (Currently Disabled)
 # DATABASES = {
@@ -121,16 +121,16 @@ DATABASES = {
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME', default='postgres'),
-#         'USER': config('DATABASE_USER', default='postgres'),
-#         'PASSWORD': config('DATABASE_PASSWORD', default=''),
-#         'HOST': config('DATABASE_HOST', default='localhost'),
-#         'PORT': config('DATABASE_PORT', default='5432'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME', default='postgres'),
+        'USER': config('DATABASE_USER', default='postgres'),
+        'PASSWORD': config('DATABASE_PASSWORD', default=''),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432'),
+    }
+}
 
 
 
